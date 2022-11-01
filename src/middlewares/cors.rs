4ff57ca77365c44ce4f0e26352ab610b6c532d0c
Victor Heliomar <victordev2002@gmail.com>
2022-11-01 14:45:16 -0400

    Added code status to the response

--- src/middlewares/cors.rs
+++ src/middlewares/cors.rs
@@ -2,7 +2,7 @@ use std::pin::Pin;
 
 use futures::{FutureExt, TryFutureExt, future};
 use gotham::handler::HandlerFuture;
-use gotham::hyper::{StatusCode, Response};
+use gotham::hyper::StatusCode;
 use gotham::hyper::header::{HeaderMap, ORIGIN};
 use gotham::middleware::Middleware;
 use gotham::prelude::*;
@@ -15,43 +15,37 @@ pub struct CorsMiddleware;
 
 impl Middleware for CorsMiddleware {
     fn call<Chain>(self, state: State, chain: Chain) -> Pin<Box<HandlerFuture>>
-    where
-        Chain: FnOnce(State) -> Pin<Box<HandlerFuture>>,
-    {
-        let allowed_origins = env::var("WHITELIST").unwrap_or_else(|_| "*".to_string());
+        where
+            Chain: FnOnce(State) -> Pin<Box<HandlerFuture>>,
+        {
+            let allowed_origins = env::var("WHITELIST").unwrap_or_else(|_| "*".to_string());
 
-        let header_map = HeaderMap::borrow_from(&state);
+            let header_map = HeaderMap::borrow_from(&state);
 
-        let requests_origin = match header_map.get(ORIGIN) {
-            Some(origin) => origin.to_str().unwrap().to_string(),
-            None => "".to_string(),
-        };
-
-        let response_allowed_origin = if allowed_origins.contains(",") {
-            allowed_origins.split(',').find(|origin| origin.trim() == &requests_origin).unwrap_or("").to_owned()
-        } else {
-            allowed_origins
-        };
-
-        let result = chain(state);
-
-        let modified_result = result.and_then(move |(state, mut response)| {
-            {
-                let headers = response.headers_mut();
-
-                headers.insert(
-                    "Access-Control-Allow-Origin",
-                    format!(
-                        "{}",
-                        response_allowed_origin,
-                    )
-                    .parse()
-                    .unwrap(),
-                );
+            let requests_origin = match header_map.get(ORIGIN) {
+                Some(origin) => origin.to_str().unwrap().to_string(),
+                None => "".to_string(),
             };
-            future::ok((state, response))
-        });
 
-        modified_result.boxed()
-    }
+            let response_allowed_origin = if allowed_origins.contains(",") {
+                allowed_origins.split(',').find(|origin| origin.trim() == &requests_origin).unwrap_or("").to_owned()
+            } else {
+                allowed_origins
+            };
+
+            let result = chain(state);
+
+            let modified_result = result.and_then(move |(state, mut response)| {
+                { 
+                    response.headers_mut().insert(
+                        "Access-Control-Allow-Origin",
+                        response_allowed_origin.parse().unwrap(),
+                    );
+                    *response.status_mut() = StatusCode::UNAUTHORIZED;
+                };
+                future::ok((state, response))
+            });
+
+            modified_result.boxed()
+        }
 }
